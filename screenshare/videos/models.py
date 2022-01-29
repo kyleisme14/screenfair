d3e6fa7ed44064d85615f52f7ad0f39ba4f5c4b5
“kyleisme14” <kylekallman@gmail.com>
2022-01-28 17:18:55 -0800

    comments

--- screenshare/videos/models.py
+++ screenshare/videos/models.py
@@ -10,3 +10,23 @@ class Video(models.Model):
 	video_file = models.FileField(upload_to='uploads/video_files', validators = [FileExtensionValidator(allowed_extensions=['mp4'])])
 	thumbnail = models.FileField(upload_to='uploads/thumbnails', validators = [FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
 	date_posted = models.DateTimeField(default=timezone.now)
+	category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+class Category(models.Model):
+	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name 
+	
+	class Meta: 
+		verbose_name = "Category"
+		verbose_name_plural = "Categories"
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	video = models.ForeignKey('Video', on_delete=models.CASCADE)
+	comment = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'User: {self.uesr} | Created On: {self.created_on.strftime("%b %d %Y %I %M %p")}'
