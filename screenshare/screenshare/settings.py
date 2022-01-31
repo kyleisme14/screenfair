adc77dd5282c998300c51000c30d799ed9c3a09f
“kyleisme14” <kylekallman@gmail.com>
2022-01-30 21:06:28 -0800

    settings

--- screenshare/screenshare/settings.py
+++ screenshare/screenshare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6b&o&jtjk+@byvp7q^4&c7l)^dwwdojz2g!24ts%7+&stx@4wr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://screenfair.herokuapp.com/]
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
